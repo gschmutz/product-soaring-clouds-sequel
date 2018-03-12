53f1a54dd12a67a54d60c40a884cc1cee5cc732e
Guido Schmutz <guido.schmutz@hotmail.com>
2018-03-12 13:07:46 +0100

    added german products

--- data/load.sh
+++ data/load.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for f in ./json/*.json; do echo "Processing $f file.."; curl -X POST -H "Content-Type: application/json" -d @$f http://$1:$2/products; done
+for f in ./json/*.json; do echo "Processing $f file.."; curl -X POST -H "Content-Type: application/json" -d @$f http://$1:$2/api/products; done
 
-for f in ./image/*.jpg; do echo "Processing $f file.."; curl -X POST -F file=@$f http://$1:$2/images; done
+for f in ./image/*.jpg; do echo "Processing $f file.."; curl -X POST -F file=@$f http://$1:$2/api/images; done
 
 
 #curl -X POST -H "Content-Type: application/json" -d @./json/ovomaltine-cocoa-powder.json http://129.150.114.134:8080/product
